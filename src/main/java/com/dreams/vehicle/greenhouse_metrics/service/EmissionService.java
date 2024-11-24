5e309d47398178756ea661f6df1938eabcbcd3ee
Rahil Khan <RahilKhan@users.noreply.github.com>
2024-11-24 15:58:32 +0530

    Update EmissionService.java

--- src/main/java/com/dreams/vehicle/greenhouse_metrics/service/EmissionService.java
+++ src/main/java/com/dreams/vehicle/greenhouse_metrics/service/EmissionService.java
@@ -4,6 +4,11 @@ import com.dreams.vehicle.greenhouse_metrics.model.VehicleEmission;
 import com.dreams.vehicle.greenhouse_metrics.repository.VehicleEmissionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +17,14 @@ public class EmissionService {
 
     private static final Logger logger = LoggerFactory.getLogger(EmissionService.class);
     private final VehicleEmissionRepository vehicleEmissionRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public EmissionService(VehicleEmissionRepository vehicleEmissionRepository) {
+    public EmissionService(VehicleEmissionRepository vehicleEmissionRepository, @Qualifier("mongoTemplate") MongoTemplate mongoTemplate) {
         this.vehicleEmissionRepository = vehicleEmissionRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
-    // @Scheduled(fixedRate = 5000) // Generate a new emission every 5 seconds
+//    @Scheduled(fixedRate = 5000) // Generate a new emission every 5 seconds
     private void generateRandomEmissions() {
         VehicleEmission emission = VehicleEmission.generateRandomEmission();
         try {
