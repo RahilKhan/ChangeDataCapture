e36554299d96baca7397ff9a94d7ca7a1ee1e785
Rahil Khan <RahilKhan@users.noreply.github.com>
2024-11-24 15:55:55 +0530

    Update VehicleEmissionRepository.java
    added method to find Vehicle by Vehicle type

--- src/main/java/com/dreams/vehicle/greenhouse_metrics/repository/VehicleEmissionRepository.java
+++ src/main/java/com/dreams/vehicle/greenhouse_metrics/repository/VehicleEmissionRepository.java
@@ -1,9 +1,11 @@
 package com.dreams.vehicle.greenhouse_metrics.repository;
 
 import com.dreams.vehicle.greenhouse_metrics.model.VehicleEmission;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehicleEmissionRepository extends MongoRepository<VehicleEmission, String> {
+  List<VehicleEmission> findByVehicleType(final String vehicleType);
 }
